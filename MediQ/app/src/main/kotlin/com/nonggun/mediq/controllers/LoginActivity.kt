4d4d57f8bf1e49a2e6a0f59a7f869fac5d519338
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-28 22:27:07 +0700

    moved LoginActivity to controllers package

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/LoginActivity.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.nonggun.mediq
+package com.nonggun.mediq.controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.nonggun.mediq.R
 
 class LoginActivity : AppCompatActivity() {
 
