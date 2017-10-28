7fdf8c593af9a7ed4a27f76514d6e125ee7318a2
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-28 23:29:59 +0700

    hid RegisterProfileActivity's action bar

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/register/RegisterProfileActivity.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/register/RegisterProfileActivity.kt
@@ -9,5 +9,6 @@ class RegisterProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_profile)
+        supportActionBar?.hide()
     }
 }
