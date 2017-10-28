95998025b11ff64870552cbbf36477003cf49a08
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-28 13:31:30 +0700

    made status bar transparent

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/LoginActivity.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.nonggun.mediq
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.nonggun.mediq.utilities.StatusBarUtils
 
 class LoginActivity : AppCompatActivity() {
 
@@ -10,6 +9,5 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
-        StatusBarUtils.fullScreenLayout(window)
     }
 }
