f15633eae624226c2aaace0a3632bd2ca6ba6081
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-28 13:16:53 +0700

    edited login layout style

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/LoginActivity.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/LoginActivity.kt
@@ -2,11 +2,14 @@ package com.nonggun.mediq
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.nonggun.mediq.utilities.StatusBarUtils
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+        StatusBarUtils.fullScreenLayout(window)
     }
 }
