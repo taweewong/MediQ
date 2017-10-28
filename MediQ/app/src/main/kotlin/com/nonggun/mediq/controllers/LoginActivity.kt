f966df38452d6c465041a630adbb1a92b780b475
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-28 23:28:54 +0700

    set registerText's onClick to go to register activity

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/LoginActivity.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/LoginActivity.kt
@@ -1,14 +1,30 @@
 package com.nonggun.mediq.controllers
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.nonggun.mediq.R
+import com.nonggun.mediq.controllers.register.RegisterProfileActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        registerText.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.registerText -> openActivity(RegisterProfileActivity().javaClass)
+        }
+    }
+
+    private fun <T>openActivity(javaClass: Class<T>) {
+        startActivity(Intent(this, javaClass))
     }
 }
