ccf1efed229a797a6a76b02d7e632a43ba21c4f2
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-10-29 14:34:31 +0700

    set registerProfileNextButton's onClick to go to register name activity

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/register/RegisterProfileActivity.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/register/RegisterProfileActivity.kt
@@ -1,14 +1,29 @@
 package com.nonggun.mediq.controllers.register
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.nonggun.mediq.R
+import kotlinx.android.synthetic.main.activity_register_profile.*
 
-class RegisterProfileActivity : AppCompatActivity() {
+class RegisterProfileActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_profile)
         supportActionBar?.hide()
+
+        registerProfileNextButton.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.registerProfileNextButton -> openActivity(RegisterProfileActivity().javaClass)
+        }
+    }
+
+    private fun <T>openActivity(javaClass: Class<T>) {
+        startActivity(Intent(this, javaClass))
     }
 }
