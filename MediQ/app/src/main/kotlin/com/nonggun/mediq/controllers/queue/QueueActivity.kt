c143465ce86647d6a82133ffc311a5028766f909
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-04 16:08:22 +0700

    create class QueueActivity

---
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/queue/QueueActivity.kt
@@ -0,0 +1,13 @@
+package com.nonggun.mediq.controllers.queue
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.nonggun.mediq.R
+
+class QueueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_queue)
+    }
+}
