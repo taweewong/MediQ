1bde3629d581e3f2d625912501a612c640f53a5a
Sanhaphat Srijumpa <fluke031@hotmail.com>
2017-12-04 14:55:59 +0700

    create user class

---
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/models/User.kt
@@ -0,0 +1,7 @@
+package com.nonggun.mediq.models
+
+public data class User(var firstName: String,
+                       var lastName: String,
+                       var citizenId: String,
+                       var phoneNumber: String,
+                       var password: String)
