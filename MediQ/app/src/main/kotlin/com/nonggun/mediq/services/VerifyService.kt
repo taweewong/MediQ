a08d60333a7d092411cf5d0d8b5bbabbf765fd74
Sanhaphat Srijumpa <fluke031@hotmail.com>
2017-12-04 17:26:26 +0700

    update VerifyService class

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/services/VerifyService.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/services/VerifyService.kt
@@ -1,7 +1,49 @@
 package com.nonggun.mediq.services
 
-/**
- * Created by fluke on 04-Dec-17.
- */
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 class VerifyService {
+    val database = FirebaseDatabase.getInstance()
+    val databaseRef = database.reference
+
+    fun verifyPhoneNumber(number: String){
+        databaseRef.child("users").orderByChild("phoneNumuber").equalTo(number)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(p0: DataSnapshot) {
+                        if (p0.children.toMutableList().size != 0) {
+                            for (phoneSnapshot: DataSnapshot in p0.children){
+                                Log.d("DEBUG SNAP", phoneSnapshot.value.toString())
+                            }
+                        } else {
+                            Log.d("DEBUG SNAP", "null")
+                        }
+                    }
+
+                    override fun onCancelled(p0: DatabaseError) {
+                        Log.e("DEBUG", p0.message);
+                    }
+        })
+    }
+
+    fun verifyCitizenId(id : String){
+        databaseRef.child("users").orderByChild("citenzenId").equalTo(id)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(p0: DataSnapshot) {
+                        if (p0.children.toMutableList().size != 0) {
+                            for (idSnapshot: DataSnapshot in p0.children){
+                                Log.d("DEBUG SNAP", idSnapshot.value.toString())
+                            }
+                        } else {
+                            Log.d("DEBUG SNAP", "null")
+                        }
+                    }
+
+                    override fun onCancelled(p0: DatabaseError?) {
+                        Log.e("DEBUG", p0.message)
+                    }
+    }
 }
