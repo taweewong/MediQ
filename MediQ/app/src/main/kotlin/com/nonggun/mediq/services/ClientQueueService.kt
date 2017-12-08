3915aeeb07d470fb44b6ad1fe6c2fd8dc25de98d
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-08 16:51:10 +0700

    add method getCurrentInProgressQueue to class ClientQueueService

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/services/ClientQueueService.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/services/ClientQueueService.kt
@@ -14,8 +14,14 @@ class ClientQueueService(private val context: Context) {
         fun onGetPreviousQueueFailed(message: String)
     }
 
+    interface OnGetCurrentInProgressQueueListener {
+        fun onGetCurrentInProgressQueueSuccess(currentInProgressQueue: String)
+        fun onGetCurrentInProgressQueueFailed(message: String)
+    }
+
     companion object {
         private val CHILD_QUEUES = "queues"
+        private val CHILD_QUEUE = "queue"
     }
 
     private val databaseRef = FirebaseDatabase.getInstance().reference.child(CHILD_QUEUES)
@@ -30,5 +36,27 @@ class ClientQueueService(private val context: Context) {
                 listener.onGetPreviousQueueFailed(context.getString(R.string.error_load_previous_queue))
             }
         })
+    }
+
+    fun getCurrentInProgressQueue(listener: OnGetCurrentInProgressQueueListener) {
+        databaseRef.limitToFirst(1).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                listener.onGetCurrentInProgressQueueSuccess(getCurrentQueueFrom(dataSnapshot))
+            }
+
+            override fun onCancelled(dataSnapshot: DatabaseError?) {
+                listener.onGetCurrentInProgressQueueFailed(context.getString(R.string.error_load_inprogress_queue))
+            }
+        })
+    }
+
+    private fun getCurrentQueueFrom(dataSnapshot: DataSnapshot): String {
+        var currentInProgressQueue = context.getString(R.string.no_previous_queue)
+
+        for (queueSnapshot in dataSnapshot.children) {
+            currentInProgressQueue = queueSnapshot.child(CHILD_QUEUE).getValue(String::class.java)
+        }
+
+        return currentInProgressQueue
     }
 }
