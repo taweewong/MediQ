905d74a08bc54e069152b75818c8c994713a1c2e
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-08 15:43:11 +0700

    add method getPreviousQueueNumber to class ClientQueueService

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/services/ClientQueueService.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/services/ClientQueueService.kt
@@ -1,12 +1,34 @@
 package com.nonggun.mediq.services
 
+import android.content.Context
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.nonggun.mediq.R
 
-class ClientQueueService {
+class ClientQueueService(private val context: Context) {
+
+    interface OnGetPreviousQueueNumberListener {
+        fun onGetPreviousQueueSuccess(previousQueueNumber: Int)
+        fun onGetPreviousQueueFailed(message: String)
+    }
 
     companion object {
         private val CHILD_QUEUES = "queues"
     }
 
     private val databaseRef = FirebaseDatabase.getInstance().reference.child(CHILD_QUEUES)
+
+    fun getPreviousQueueNumber(listener: OnGetPreviousQueueNumberListener) {
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                listener.onGetPreviousQueueSuccess(dataSnapshot.children.count())
+            }
+
+            override fun onCancelled(dataSnapshot: DatabaseError?) {
+                listener.onGetPreviousQueueFailed(context.getString(R.string.error_load_previous_queue))
+            }
+        })
+    }
 }
