c20d1443879f38e3b9ec78df2eea9b6a8fa7475b
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-12 00:35:01 +0700

    add method register in class RegisterFacade

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/facades/RegisterFacade.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/facades/RegisterFacade.kt
@@ -1,5 +1,7 @@
 package com.nonggun.mediq.facades
 
+import com.nonggun.mediq.models.User
+import com.nonggun.mediq.services.RegisterService
 import com.nonggun.mediq.services.VerifyService
 import com.nonggun.mediq.services.VerifyService.OnVerifyRegisterDataComplete
 
@@ -8,5 +10,9 @@ object RegisterFacade {
     fun verifyDuplicateRegisterInput(phoneNumber: String, citizenId: String, listener: OnVerifyRegisterDataComplete) {
         VerifyService.verifyPhoneNumber(phoneNumber, listener)
         VerifyService.verifyCitizenId(citizenId, listener)
+    }
+
+    fun register(user: User) {
+        RegisterService.register(user)
     }
 }
