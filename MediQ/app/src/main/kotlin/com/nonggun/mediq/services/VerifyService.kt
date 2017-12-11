29a79609f11f24f0b6a5a8a0cad39255988ac0a2
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-11 14:55:25 +0700

    refactor class VerifyService

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/services/VerifyService.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/services/VerifyService.kt
@@ -6,16 +6,20 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class VerifyService {
-    val database = FirebaseDatabase.getInstance()
-    val databaseRef = database.reference
+object VerifyService {
+    private val databaseRef = FirebaseDatabase.getInstance().reference
 
-    fun verifyPhoneNumber(number: String){
-        databaseRef.child("users").orderByChild("phoneNumuber").equalTo(number)
+    private val CHILD_USERS = "users"
+    private val CHILD_PHONE_NUMBER = "phoneNumber"
+    private val CHILD_CITIZEN_ID = "citizenId"
+
+    fun verifyPhoneNumber(number: String) {
+        databaseRef.child(CHILD_USERS).orderByChild(CHILD_PHONE_NUMBER).equalTo(number)
                 .addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onDataChange(p0: DataSnapshot) {
                         if (p0.children.toMutableList().size != 0) {
-                            for (phoneSnapshot: DataSnapshot in p0.children){
+                            for (phoneSnapshot: DataSnapshot in p0.children) {
+                                //TODO: implemnt
                                 Log.d("DEBUG SNAP", phoneSnapshot.value.toString())
                             }
                         } else {
@@ -24,17 +28,17 @@ class VerifyService {
                     }
 
                     override fun onCancelled(p0: DatabaseError) {
-                        Log.e("DEBUG", p0.message);
+                        Log.e("DEBUG", p0.message)
                     }
-        })
+                })
     }
 
-    fun verifyCitizenId(id : String){
-        databaseRef.child("users").orderByChild("citenzenId").equalTo(id)
+    fun verifyCitizenId(id: String) {
+        databaseRef.child(CHILD_USERS).orderByChild(CHILD_CITIZEN_ID).equalTo(id)
                 .addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(p0: DataSnapshot) {
-                        if (p0.children.toMutableList().size != 0) {
-                            for (idSnapshot: DataSnapshot in p0.children){
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.children.toMutableList().size != 0) {
+                            for (idSnapshot: DataSnapshot in dataSnapshot.children) {
                                 Log.d("DEBUG SNAP", idSnapshot.value.toString())
                             }
                         } else {
@@ -42,8 +46,9 @@ class VerifyService {
                         }
                     }
 
-                    override fun onCancelled(p0: DatabaseError?) {
-                        Log.e("DEBUG", p0.message)
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        Log.e("DEBUG", databaseError.message)
                     }
+                })
     }
 }
