ef092573bd8865853aee8c81f02e58cce92cd94a
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-11 16:13:20 +0700

    class User implement Parcelable

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/models/User.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/models/User.kt
@@ -1,7 +1,40 @@
 package com.nonggun.mediq.models
 
-data class User(var firstName: String = "",
-                var lastName: String = "",
-                var citizenId: String = "",
-                var phoneNumber: String = "",
-                var password: String = "")
+import android.os.Parcel
+import android.os.Parcelable
+
+data class User(private var firstName: String = "",
+                private var lastName: String = "",
+                private var citizenId: String = "",
+                private var phoneNumber: String = "",
+                private var password: String = "") : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(firstName)
+        parcel.writeString(lastName)
+        parcel.writeString(citizenId)
+        parcel.writeString(phoneNumber)
+        parcel.writeString(password)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
