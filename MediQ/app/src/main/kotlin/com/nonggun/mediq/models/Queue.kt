6bacec0f5f29fbaa226d7d75b07f206c4edadf8f
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-12 15:53:23 +0700

    add property userId to class Queue

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/models/Queue.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/models/Queue.kt
@@ -5,21 +5,24 @@ import android.os.Parcelable
 
 data class Queue(var queueNumber: Int,
                  var name: String,
-                 var type: String) : Parcelable {
+                 var type: String,
+                 var userId: String) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readInt(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString())
 
     enum class queueType {
         APPLICATION, WALK_IN
     }
 
-    constructor(parcel: Parcel) : this(
-            parcel.readInt(),
-            parcel.readString(),
-            parcel.readString())
-
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(queueNumber)
         parcel.writeString(name)
         parcel.writeString(type)
+        parcel.writeString(userId)
     }
 
     override fun describeContents(): Int {
@@ -35,4 +38,6 @@ data class Queue(var queueNumber: Int,
             return arrayOfNulls(size)
         }
     }
+
+
 }
