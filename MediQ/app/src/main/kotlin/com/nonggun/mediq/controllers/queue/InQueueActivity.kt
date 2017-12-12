77f475288035ad5539d856078b454dc60ad2ec79
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-12 22:04:26 +0700

    create class InQueueActivity

---
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/queue/InQueueActivity.kt
@@ -0,0 +1,62 @@
+package com.nonggun.mediq.controllers.queue
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.nonggun.mediq.R
+import com.nonggun.mediq.base.BaseActivity
+import com.nonggun.mediq.facades.ClientQueueFacade
+import com.nonggun.mediq.models.User
+import com.nonggun.mediq.models.User.Key.USER_PARCEL_KEY
+import com.nonggun.mediq.services.ClientInQueueService.OnGetUserQueueDataListener
+import com.nonggun.mediq.services.ClientInQueueService.OnGetUserQueueListener
+import kotlinx.android.synthetic.main.activity_in_queue.*
+
+class InQueueActivity : BaseActivity(), OnGetUserQueueDataListener, OnGetUserQueueListener{
+
+    private lateinit var user: User
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_in_queue)
+        supportActionBar?.hide()
+
+        user = intent.getParcelableExtra(USER_PARCEL_KEY)
+        ClientQueueFacade.getInQueueData(this, user, this, this)
+    }
+
+    override fun onGetCurrentQueueSuccess(user: User, userQueue: Int) {
+        userQueueText.text = userQueue.toString()
+    }
+
+    override fun onGetCurrentQueueFailed(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onGetCurrentQueueNotFound(user: User) {
+        startQueueActivity(user)
+    }
+
+    override fun onGetCurrentInProgressQueueSuccess(queueNumber: String) {
+        currentQueueText.text = queueNumber
+    }
+
+    override fun onGetCurrentInProgressQueueFailed(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onGetWaitQueueNumberAndTimeSuccess(previousQueueNumber: Int, waitTime: String) {
+        waitQueueText.text = previousQueueNumber.toString()
+        waitTimeText.text = waitTime
+    }
+
+    override fun onGetWaitQueueNumberAndTimeFailed(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startQueueActivity(user: User) {
+        val intent = Intent(this, QueueActivity::class.java)
+        intent.putExtra(USER_PARCEL_KEY, user)
+        startActivity(intent)
+    }
+}
