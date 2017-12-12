3e15c950a35355e4d3d7590f149d9df114de1b22
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-12 22:05:20 +0700

    add method getInQueueData in class ClientQueueFacade

--- MediQ/app/src/main/kotlin/com/nonggun/mediq/facades/ClientQueueFacade.kt
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/facades/ClientQueueFacade.kt
@@ -7,6 +7,10 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.nonggun.mediq.R
+import com.nonggun.mediq.models.User
+import com.nonggun.mediq.services.ClientInQueueService
+import com.nonggun.mediq.services.ClientInQueueService.OnGetUserQueueDataListener
+import com.nonggun.mediq.services.ClientInQueueService.OnGetUserQueueListener
 import com.nonggun.mediq.services.ClientQueueService
 import com.nonggun.mediq.services.ClientQueueService.OnGetQueueDataListener
 
@@ -23,6 +27,23 @@ object ClientQueueFacade {
                 ClientQueueService.getCurrentInProgressQueue(context, onGetQueueDataListener)
                 ClientQueueService.getPreviousQueueNumber(context, onGetQueueDataListener)
                 ClientQueueService.getWaitTime(context, onGetQueueDataListener)
+            }
+
+            override fun onCancelled(p0: DatabaseError?) {
+                Toast.makeText(context, context.getString(R.string.error_load_data), Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    fun getInQueueData(context: Context, user: User, onGetUserQueueListener: OnGetUserQueueListener,
+                       onGetUserQueueDataListener: OnGetUserQueueDataListener) {
+        val databaseRef = FirebaseDatabase.getInstance().reference.child(CHILD_QUEUES)
+
+        databaseRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot?) {
+                ClientInQueueService.getUserQueue(user, onGetUserQueueListener)
+                ClientInQueueService.getCurrentInProgressQueue(context, onGetUserQueueDataListener)
+                ClientInQueueService.getWaitQueueNumberAndTime(user.userId, onGetUserQueueDataListener)
             }
 
             override fun onCancelled(p0: DatabaseError?) {
