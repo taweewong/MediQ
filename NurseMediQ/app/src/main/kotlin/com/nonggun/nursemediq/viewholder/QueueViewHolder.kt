ec97929013426e6d8c3cf9741ddbd3494a079402
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-13 22:10:43 +0700

    bind delete button in class QueueViewHolder

--- NurseMediQ/app/src/main/kotlin/com/nonggun/nursemediq/viewholder/QueueViewHolder.kt
+++ NurseMediQ/app/src/main/kotlin/com/nonggun/nursemediq/viewholder/QueueViewHolder.kt
@@ -2,6 +2,7 @@ package com.nonggun.nursemediq.viewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.nonggun.nursemediq.R
 
@@ -9,5 +10,6 @@ class QueueViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var queueText: TextView = itemView.findViewById(R.id.queueText)
     var clientNameText: TextView = itemView.findViewById(R.id.clientNameText)
     var clientPhoneNumberText: TextView = itemView.findViewById(R.id.clientPhoneNumberText)
+    var deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
 }
 
