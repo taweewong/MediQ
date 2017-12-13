18c733737c0dacd0cfe27e7ee5469ed1b0939d29
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-13 15:39:33 +0700

    create class SplashScreenActivity

---
+++ MediQ/app/src/main/kotlin/com/nonggun/mediq/controllers/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.nonggun.mediq.controllers.splashscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.nonggun.mediq.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
