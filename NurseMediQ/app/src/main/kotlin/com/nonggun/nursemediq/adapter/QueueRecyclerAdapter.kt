8fbe1d83d3ecc4d493b4a67b5b4dadd29f1609d4
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-13 20:00:44 +0700

    create class QueueRecyclerAdapter

---
+++ NurseMediQ/app/src/main/kotlin/com/nonggun/nursemediq/adapter/QueueRecyclerAdapter.kt
@@ -0,0 +1,30 @@
+package com.nonggun.nursemediq.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.nonggun.nursemediq.model.Queue
+import com.nonggun.nursemediq.viewholder.QueueViewHolder
+import android.view.LayoutInflater
+import com.nonggun.nursemediq.R
+
+
+class QueueRecyclerAdapter(private val queues: List<Queue>): RecyclerView.Adapter<QueueViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QueueViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = inflater.inflate(R.layout.queue_view_holder, parent, false)
+
+        return QueueViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: QueueViewHolder, position: Int) {
+        holder.queueText.text = queues[position].queueNumber.toString()
+        holder.clientNameText.text = queues[position].name
+        holder.clientPhoneNumberText.text = queues[position].phoneNumber
+    }
+
+    override fun getItemCount(): Int {
+        return queues.size
+    }
+
+}
