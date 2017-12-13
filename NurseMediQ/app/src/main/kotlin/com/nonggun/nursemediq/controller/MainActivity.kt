52cea083e347b11e000be25f82e5b0305cae9870
Taweewong Tocharoen <it58070045@office365.it.kmitl.ac.th>
2017-12-13 22:14:28 +0700

    rename package dialog to dialogs

--- NurseMediQ/app/src/main/kotlin/com/nonggun/nursemediq/controller/MainActivity.kt
+++ NurseMediQ/app/src/main/kotlin/com/nonggun/nursemediq/controller/MainActivity.kt
@@ -7,7 +7,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
 import com.nonggun.nursemediq.R
 import com.nonggun.nursemediq.adapter.QueueRecyclerAdapter
-import com.nonggun.nursemediq.dialog.AddQueueDialog
+import com.nonggun.nursemediq.dialogs.AddQueueDialog
 import com.nonggun.nursemediq.model.Queue
 import com.nonggun.nursemediq.service.WalkInQueueService
 import kotlinx.android.synthetic.main.activity_main.*
--- NurseMediQ/app/src/main/kotlin/com/nonggun/nursemediq/dialogs/AddQueueDialog.kt
+++ NurseMediQ/app/src/main/kotlin/com/nonggun/nursemediq/dialogs/AddQueueDialog.kt
@@ -1,4 +1,4 @@
-package com.nonggun.nursemediq.dialog
+package com.nonggun.nursemediq.dialogs
 
 import android.app.Activity
 import android.app.AlertDialog
